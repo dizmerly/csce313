db21e39ce4f3d144202f5c811f1c527556de796e
Daniel Izmerly <dan.izmerly@gmail.com>
2026-09-13 22:54:19 -0500

    added compilesornot, and hangman game to the directory, just have to work on them and finish them

---
+++ hangman_game/src/main.rs
@@ -0,0 +1,47 @@
+extern crate rand;
+
+use rand::prelude::*;
+use std::fs;
+use std::io;
+use std::io::Write;
+
+const NUM_INCORRECT_GUESSES: u32 = 5;
+const WORDS_PATH: &str = "words.txt";
+
+fn pick_a_random_word() -> String {
+  let file_string = fs::read_to_string(WORDS_PATH).expect("Unable to read file.");
+  let words: Vec<&str> = file_string.split('\n').collect();
+  String::from(words[rand::rng().random_range(0..words.len())].trim())
+}
+
+fn print_chars(ch: &Vec<char>) {
+  // print all the characters in the vector
+}
+
+fn num_valid_chars(ch: &Vec<char>) -> usize {
+  // number of characters in the vector that are not '_'
+}
+
+fn main() {
+  let secret_word = pick_a_random_word();
+  let secret_word_chars: Vec<char> = secret_word.chars().collect();
+  println!("random word: {}", secret_word);
+
+  let mut guesses_left = NUM_INCORRECT_GUESSES;
+
+  while guesses_left > 0 {
+      // print the characters in the current guess
+      // print # of guesses left
+      print!("Please guess a letter: ");
+      io::stdout().flush().unwrap();
+
+      let mut input = String::new();
+      io::stdin().read_line(&mut input).expect("Failed to read line");
+      let input_char = input.chars().next().unwrap();
+
+      // adjust guessed_letters, current guess
+      // adjust guesses_left
+      // return if successfully guessed word
+  }
+  // print failure message
+}
